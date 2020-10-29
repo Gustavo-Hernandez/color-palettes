a3eb9ca4e7646e00a24d28d856db89a5916382ba
Gustavo Hernández <47768780+Gustavo-Hernandez@users.noreply.github.com>
2020-10-29 12:25:34 -0600

    Added palette

---
+++ colors-app/src/Palette.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Palette = (props) => {
+ return <div className="Palette">
+    {/*Navbar goes here*/}
+    <div className="Palette-colors">
+    {/*Color boxes go here*/}
+    </div>
+    {/*Footer goes here*/}
+ </div>
+}
+
+export default Palette;
