328693508a5b3c65e459ce589c318888ba90c306
Gustavo Hernández <47768780+Gustavo-Hernandez@users.noreply.github.com>
2020-10-29 22:50:51 -0600

    Styled ColorBox

--- colors-app/src/ColorBox.js
+++ colors-app/src/ColorBox.js
@@ -1,10 +1,15 @@
 import React from "react";
-import "./ColorBox.css"
+import "./ColorBox.css";
 const ColorBox = ({ background, name }) => {
   return (
-    <div style={{ background: background }} className="ColorBox">
-      <span>{name}</span>
-      <span>MORE</span>
+    <div style={{ background }} className="ColorBox">
+      <div className="copy-container">
+        <div className="box-content">
+          <span>{name}</span>
+        </div>
+        <button className="copy-button">Copy</button>
+      </div>
+      <span className="see-more">More</span>
     </div>
   );
 };
